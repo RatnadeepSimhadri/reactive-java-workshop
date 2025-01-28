3190bf482562cc0ba574bf6ae050cec1b41a3abf
Ratnadeep Simhadri <ratnadeep.ms@outlook.com>
2025-01-28 15:54:09 -0800

    Feat[Exercise-2]: Reactive Programming Excercise

--- src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -4,18 +4,28 @@ import java.io.IOException;
 
 public class Exercise2 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().subscribe(element -> {
+            System.out.println(Thread.currentThread().getName()+" is logging "+ element);
+        });
 
         // Print all users in the ReactiveSources.userFlux stream
-        // TODO: Write code here
+        ReactiveSources.userFlux().subscribe(element -> {
+            System.out.println(Thread.currentThread().getName()+" is logging "+ element);
+        });
 
         System.out.println("Press a key to end");
-        System.in.read();
+        System.in.read();  // Waiting for User Action so that Stream can flow. Thread.sleep(15000); // wait 15 seconds
     }
+
+    /**
+     * 
+     * Key Take Away is Reactive Programming can be Async. Reactive is just Push Based Programming Model 
+     * The Flux might implement the Callback / Lambda in a separate thread or Main Thread 
+     */
 
 }
