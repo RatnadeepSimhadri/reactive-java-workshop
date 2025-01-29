4255bee6b21bc9f9a55ff0a592bb315ac07bab29
Ratnadeep Simhadri <ratnadeep.ms@outlook.com>
2025-01-29 12:45:19 -0800

    Generating Mono from Flux

--- src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
@@ -11,12 +11,15 @@ public class Exercise9 {
 
         // Print size of intNumbersFlux after the last item returns
         // TODO: Write code here
-
+        // Answer: ReactiveSources.intNumbersFlux().count().subscribe(count -> System.out.println(count));
+        
         // Collect all items of intNumbersFlux into a single list and print it
         // TODO: Write code here
+        ReactiveSources.intNumbersFlux().collectList().subscribe(System.out::println);
 
         // Transform to a sequence of sums of adjacent two numbers
         // TODO: Write code here
+        System.out.println("Continuing the Main Thread");
 
         System.out.println("Press a key to end");
         System.in.read();
